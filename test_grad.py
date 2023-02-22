af3a99ef9949cf50c48664b74e9aeab26e27919c
Sina Zendehroud <zendehroud@physik.fu-berlin.de>
2023-02-22 17:19:48 +0100

    rename grad_check

--- test_grad.py
+++ test_grad.py
@@ -12,7 +12,7 @@ A, _ = lattice.links_matrix(dim)
 l = lattice.create_ll(dim)
 
 # uncomment to check stretched lattice
-P, box = lattice.stretch_it(P, box, 0.8)
+# P, box = lattice.stretch_it(P, box, 0.8)
 
 err = check_grad(energy.energy, energy.gradient, P.ravel(), box, A, l)
 print(err)
